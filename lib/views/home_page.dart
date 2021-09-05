09db41e5f5b20f8c8554fdd5d822509ed6c500a0
Maxvyr <maxvy_@naver.com>
2021-09-05 22:35:25 +0200

    bottom nav bar first

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,21 +1,33 @@
-
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _index = 0;
+  final _widgetOptions = <Widget>[
+    const Text(
+      "Index 0 : HOME",
+    ),
+    const Text(
+      'Index 1: MOON',
+    ),
+    const Text(
+      'Index 2: MEDITATION',
+    ),
+    const Text(
+      'Index 3: FAVORITE',
+    ),
+    const Text(
+      'Index 4: PROFIl',
+    ),
+  ];
 
-  void _incrementCounter() {
+  void _itemTapped(int index) {
     setState(() {
-      _counter++;
+      _index = index;
     });
   }
 
@@ -23,26 +35,43 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: const Text(''),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
+        child: _widgetOptions.elementAt(_index),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+            backgroundColor: Color(0xff4b6f4d),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.dark_mode),
+            label: "Moon",
+            backgroundColor: Color(0xff4b6f4d),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.spa),
+            label: "Meditation",
+            backgroundColor: Color(0xff4b6f4d),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite_border),
+            label: "Favorite",
+            backgroundColor: Color(0xff4b6f4d),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: "Profil",
+            backgroundColor: Color(0xff4b6f4d),
+          ),
+        ],
+        currentIndex: _index,
+        selectedItemColor: Colors.white,
+        backgroundColor: Colors.white,
+        onTap: _itemTapped,
       ),
     );
   }
