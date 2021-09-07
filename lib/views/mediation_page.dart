2010f43d8edd0cd22ca004aef2a7673d3a9d43c5
Maxvyr <maxvy_@naver.com>
2021-09-07 22:44:47 +0200

    add page meditation

---
+++ lib/views/mediation_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class MeditationPage extends ConsumerWidget {
+  const MeditationPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, ScopedReader watch) {
+    // User Hero pour open Page
+    return Container(
+      child: null,
+    );
+  }
+}
