1466c2549cd444d0cd95603204ade929e890759b
Maxvyr <maxvid30@gmail.com>
2021-09-09 17:05:45 +0200

    :lipstick: design textfild search bar

--- lib/views/mediations_page.dart
+++ lib/views/mediations_page.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:line_icons/line_icons.dart';
+import 'package:meditation_design_app/controller/custom_colors.dart';
 import 'package:meditation_design_app/repository/dummy_list.dart';
 import 'package:meditation_design_app/views/widget/card_tile.dart';
 import 'package:meditation_design_app/views/widget/text_custom.dart';
 
-class MeditationsPage extends StatelessWidget {
-  const MeditationsPage({Key? key}) : super(key: key);
+class MeditationsPage extends StatefulWidget {
+  const MeditationsPage();
+
+  @override
+  State<MeditationsPage> createState() => _MeditationsPageState();
+}
+
+class _MeditationsPageState extends State<MeditationsPage> {
+  late TextEditingController controller;
+  final radiusTextField = BorderRadius.circular(10.0);
+  final paddingRightAndLeft = 15.0;
+
+  @override
+  void initState() {
+    controller = TextEditingController();
+  }
 
   @override
   Widget build(BuildContext contexth) {
@@ -13,7 +29,7 @@ class MeditationsPage extends StatelessWidget {
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: const EdgeInsets.only(left: 15.0),
+          padding: EdgeInsets.only(left: paddingRightAndLeft),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -38,11 +54,27 @@ class MeditationsPage extends StatelessWidget {
               const SizedBox(
                 height: 10.0,
               ),
-              Container(
-                height: 50.0,
-                width: double.infinity,
-                color: Colors.amberAccent,
-              ),
+              Padding(
+                padding: EdgeInsets.only(right: paddingRightAndLeft),
+                child: TextField(
+                  controller: controller,
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                        borderRadius: radiusTextField,
+                      ),
+                      hintText: "Find your sound",
+                      suffixIcon: Container(
+                          margin: const EdgeInsets.only(right: 8.0),
+                          decoration: BoxDecoration(
+                            color: CustomColor.greenMain,
+                            borderRadius: radiusTextField,
+                          ),
+                          child: const Icon(
+                            LineIcons.search,
+                            color: CustomColor.white,
+                          ))),
+                ),
+              )
             ],
           ),
         ),
